696451d9ec6212921af1ed02871ed24aa02f2a07
Derekdog689 <Derek@DssEnterprisesusa.llc>
2026-08-26 14:56:01 -0400

    Enable participant talk-to-text fields

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata, Viewport } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import ParticipantSpeechInput from "./ParticipantSpeechInput";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -69,7 +70,10 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="flex min-h-full flex-col">{children}</body>
+      <body className="flex min-h-full flex-col">
+        {children}
+        <ParticipantSpeechInput />
+      </body>
     </html>
   );
 }
